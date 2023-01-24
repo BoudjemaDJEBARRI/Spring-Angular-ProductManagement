d3db97a4d0ffe7f4fb0a6cc3d362d918b2f3a896
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-01-24 20:44:55 +0100

    Update Repository

--- src/main/java/com/djebarri/dao/ProduitRepository.java
+++ src/main/java/com/djebarri/dao/ProduitRepository.java
@@ -1,6 +1,8 @@
 package com.djebarri.dao;
 
 import com.djebarri.enteties.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -15,6 +17,6 @@ public interface ProduitRepository extends JpaRepository <Produit, Long> {
     @RestResource(path = "/byDesignation")
     public List<Produit> findByDesignationContains(@Param("mc") String des);
 
-   //@RestResource(path = "/byDesignationPage")
-   //public Page<Produit> findByDesignationContains(@Param("mc") String des, Pageable pageable);
+   @RestResource(path = "/byDesignationPage")
+   public Page<Produit> findByDesignationContains(@Param("mc") String des, Pageable pageable);
 }
